e8b7215f51737458fc1ac5ba73d32841d68f69dd
niilliiz <n.vatanara@mydoki.com>
2024-10-07 08:03:43 +0330

    feat: clark and canvas configs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import ConvexClerkClientProvider from "@/app/providers/ConvexClerkClientProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,7 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ConvexClerkClientProvider>{children}</ConvexClerkClientProvider>
       </body>
     </html>
   );

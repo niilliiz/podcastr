6cdaede4f73985e089c00247f79d1495c0d944d6
niilliiz <n.vatanara@mydoki.com>
2024-11-07 10:59:20 +0330

    generate

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -26,6 +26,8 @@ import { useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
 import GeneratePodcast from "@/components/GeneratePodcast";
 import GenerateThumbnail from "@/components/GenerateThumbnail";
+import { Button } from "@/components/ui/button";
+import { Loader } from "lucide-react";
 
 const formSchema = z.object({
   podcastTitle: z.string().min(2),
@@ -35,7 +37,22 @@ const formSchema = z.object({
 const voiceCategories = ["alloy", "shimmer", "nova", "echo", "fable", "onyx"];
 
 export default function CreatePodcast() {
+  const [imagePrompt, setImagePrompt] = useState("");
+  const [imageStorageId, setImageStorageId] = useState<Id<"_storage"> | null>(
+    null,
+  );
+  const [imageUrl, setImageUrl] = useState("");
+
+  const [audioUrl, setAudioUrl] = useState("");
+  const [audioStorageId, setAudioStorageId] = useState<Id<"_storage"> | null>(
+    null,
+  );
+  const [audioDuration, setAudioDuration] = useState(0);
+
   const [voiceType, setVoiceType] = useState<string | null>(null);
+  const [voicePrompt, setVoicePrompt] = useState("");
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -135,8 +152,37 @@ export default function CreatePodcast() {
             />
           </div>
           <div className="flex flex-col pt-10">
-            <GeneratePodcast />
-            <GenerateThumbnail />
+            <GeneratePodcast
+              setAudioStorageId={setAudioStorageId}
+              setAudio={setAudioUrl}
+              voiceType={voiceType!}
+              audio={audioUrl}
+              voicePrompt={voicePrompt}
+              setVoicePrompt={setVoicePrompt}
+              setAudioDuration={setAudioDuration}
+            />
+            <GenerateThumbnail
+              setImage={setImageUrl}
+              setImageStorageId={setImageStorageId}
+              image={imageUrl}
+              imagePrompt={imagePrompt}
+              setImagePrompt={setImagePrompt}
+            />
+            <div className="mt-10 w-full">
+              <Button
+                type="submit"
+                className="text-16 w-full bg-orange-1 py-4 font-extrabold text-white-1 transition-all duration-500 hover:bg-black-1"
+              >
+                {isSubmitting ? (
+                  <>
+                    Submitting
+                    <Loader size={20} className="animate-spin ml-2" />
+                  </>
+                ) : (
+                  "Submit & Publish Podcast"
+                )}
+              </Button>
+            </div>
           </div>
         </form>
       </Form>
--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -1,3 +1,118 @@
-export default function GeneratePodcast() {
-  return <div>generate podcast</div>;
+import { GeneratePodcastProps } from "@/types";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Loader } from "lucide-react";
+import { useState } from "react";
+import { useAction, useMutation } from "convex/react";
+import { generateAudioAction } from "@/convex/openai";
+import { api } from "@/convex/_generated/api";
+import { v4 as uuidv4 } from "uuid";
+import { useUploadFiles } from "@xixixao/uploadstuff/react";
+import { generateUploadUrl } from "@/convex/files";
+
+const useGeneratePodcast = ({
+  voiceType,
+  setAudio,
+  audio,
+  setAudioStorageId,
+  voicePrompt,
+  setVoicePrompt,
+  setAudioDuration,
+}: GeneratePodcastProps) => {
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
+  const { startUpload } = useUploadFiles(generateUploadUrl);
+
+  const getPodcastAudio = useAction(api.openai.generateAudioAction);
+
+  const getAudioUrl = useMutation(api.podcasts.getUrl);
+
+  async function generatePodcast() {
+    setIsGenerating(true);
+    setAudio("");
+
+    if (!voicePrompt) {
+      //todo: show error message
+      setIsGenerating(false);
+    }
+
+    try {
+      const response = await getPodcastAudio({
+        voice: voiceType,
+        input: voicePrompt,
+      });
+
+      const blob = new Blob([response], { type: "audio/mpeg" });
+      const fileName = `podcast-${uuidv4()}.mp4`;
+      const file = new File([blob], fileName, { type: "audio/mpeg" });
+
+      const uploaded = await startUpload([file]);
+      const storageId = (uploaded[0].response as any).storageId;
+
+      setAudioStorageId(storageId);
+
+      const audioUrl = await getAudioUrl({ storageId });
+
+      setAudio(audioUrl!);
+      setIsGenerating(false);
+
+      //todo: show success message
+    } catch (error) {
+      //todo -> toast
+      console.log("Error generating podcast", error);
+      setIsGenerating(false);
+    }
+  }
+
+  return { isGenerating, generatePodcast };
+};
+
+export default function GeneratePodcast(props: GeneratePodcastProps) {
+  const { isGenerating, generatePodcast } = useGeneratePodcast(props);
+
+  return (
+    <div>
+      <div className="flex flex-col gap-2.5">
+        <Label className="text-16 font-bold text-white-1">
+          AI Prompt to generate Podcast
+        </Label>
+        <Textarea
+          className="input-class font-light focus-visible:ring-offset-orange-1"
+          placeholder="Provide text to generate audio"
+          rows={5}
+          value={props.voicePrompt}
+          onChange={(e) => props.setVoicePrompt(e.target.value)}
+        />
+      </div>
+      <div className="mt-5 w-full max-w-[200px]">
+        <Button
+          type="submit"
+          className="text-16 bg-orange-1 py-4 font-bold text-white-1"
+          // onClick={generatePodcast}
+        >
+          {isGenerating ? (
+            <>
+              Generating
+              <Loader size={20} className="animate-spin ml-2" />
+            </>
+          ) : (
+            "Generate"
+          )}
+        </Button>
+      </div>
+      {props.audio && (
+        <audio
+          controls
+          src={props.audio}
+          autoPlay
+          className="mt-5"
+          onLoadedMetadata={(e) =>
+            props.setAudioDuration(e.currentTarget.duration)
+          }
+        />
+      )}
+    </div>
+  );
 }

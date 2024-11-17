5497037dce60e04124017c3825450b6d3de9903c
niilliiz <n.vatanara@mydoki.com>
2024-11-17 10:13:51 +0330

    refactor: providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import ConvexClerkClientProvider from "@/app/providers/ConvexClerkClientProvider";
+import ConvexClerkClientProvider from "@/providers/ConvexClerkClientProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",

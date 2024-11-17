09799ddd42e2d37372b4d21c1f98e0e5cac9b45f
niilliiz <n.vatanara@mydoki.com>
2024-11-17 10:43:58 +0330

    fix: ts

--- types/index.ts
+++ types/index.ts
@@ -80,14 +80,14 @@ export interface LatestPodcastCardProps {
 }
 
 export interface PodcastDetailPlayerProps {
-  audioUrl: string;
+  audioUrl?: string;
   podcastTitle: string;
   author: string;
   isOwner: boolean;
-  imageUrl: string;
+  imageUrl?: string;
   podcastId: Id<"podcasts">;
-  imageStorageId: Id<"_storage">;
-  audioStorageId: Id<"_storage">;
+  imageStorageId?: Id<"_storage">;
+  audioStorageId?: Id<"_storage">;
   authorImageUrl: string;
   authorId: string;
 }
